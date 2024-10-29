c6925060d74cb57517fab821615021cee58a98c2
Matthias Cullmann <matthias.cullmann@baloise.com>
2024-10-29 09:35:58 +0100

    FIX rename src

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,4 +1,3 @@
-alias pip-install='pip install --trusted-host pypi.org --trusted-host pypi.python.org --trusted-host files.pythonhosted.org'
-pip-install --upgrade build
+pip install --trusted-host pypi.org --trusted-host pypi.python.org --trusted-host files.pythonhosted.org --upgrade build
 python -m build
-pip-install --upgrade --editable .
+pip install --trusted-host pypi.org --trusted-host pypi.python.org --trusted-host files.pythonhosted.org --upgrade --editable .
