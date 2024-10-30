486e98d9fecad46b0796e57b67cf217244620295
robbizbal <roberto.galvagno@baloise.com>
2024-10-30 11:41:49 +0100

    Update entrypoint.sh and Makefile
    - remove echo in entrypoint
    - change to --without dev
    - change favicon to svg

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
 
-# Your entry point logic here
-echo "Running entry point script..."
-
 # Execute the command passed to the container
 echo "$(cat banner.txt)"
 export PATH="$HOME/.local/bin:$PATH"
