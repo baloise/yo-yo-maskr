cccd08004d9eaaba4b45754440272dc86028e793
robbizbal <roberto.galvagno@baloise.com>
2024-10-30 08:20:37 +0100

    add template

--- src/gui/landing.py
+++ src/gui/landing.py
@@ -1,9 +1,14 @@
 import fastapi
 from fastapi.responses import HTMLResponse
 from fastapi import Request, routing
+from fastapi.templating import Jinja2Templates
+
+templates = Jinja2Templates(directory="src/templates")
 
 router = fastapi.APIRouter()
 
 @router.get("/", response_class=HTMLResponse, include_in_schema=False)
-def landing(): 
-    return "<h1>hola mundo</h1>"
+def landing(request: Request): 
+    return templates.TemplateResponse(
+        request=request, name="form.html", context={"id": id}
+    )
