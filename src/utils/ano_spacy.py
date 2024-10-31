64b292e02d22d0b6485fe629305f7819c449fb55
Matthias Cullmann <matthias.cullmann@baloise.com>
2024-10-31 23:09:34 +0100

    REF remove presidio and test case working :-)

--- src/utils/ano_spacy.py
+++ src/utils/ano_spacy.py
@@ -1,9 +1,5 @@
 import spacy
 import spacy.cli
-from presidio_analyzer import AnalyzerEngine, RecognizerRegistry
-from presidio_analyzer.nlp_engine import SpacyNlpEngine, NlpEngineProvider
-from presidio_analyzer.predefined_recognizers import SpacyRecognizer
-from presidio_analyzer import PatternRecognizer
 import sys
 
 class Anon_Spacy:
@@ -12,7 +8,7 @@ class Anon_Spacy:
         size = "lg"
         gernres = {lang: "web" if lang == 'en' else "news" for lang in languages}
         self.models = {lang: f"{lang}_core_{gernres[lang]}_{size}" for lang in languages}
-        self.models_loaded = []
+        self.models_loaded = {}
 
     def _create_dict(self, text:str, analysis:list)->dict:
         replace_dict:dict = {}
@@ -30,46 +26,39 @@ class Anon_Spacy:
     def analyze_text(self, text, language='de',entities=['PERSON']):
         if not language in self.models:
             print(f"WARN: language '{language}' not supported. Supported languages are {self.models.keys()}.")
-        analysis = self._get_analyzer(language, entities).analyze(text=text+'.', language=language, entities=["PERSON"])
+        analysis = self._get_analyzer(language)(text)
         results = []
-        for result in analysis:
-            results.append({
-            "start": result.start,
-            "end": result.end,
-            "entity_type": result.entity_type,
-            "score": result.score,
-            "entity": text[result.start:result.end]
-            })
+        for result in analysis.ents:
+            if(result.label_ in entities):
+                results.append({
+                "start": result.start_char,
+                "end": result.end_char,
+                "entity_type": result.label_,
+                "entity": result.ents[0] #text[result.start_char:result.end_char]
+                })
         return results
     
-    def _get_analyzer(self,language='de',entities=['PERSON']):
-        self._ensure_model_loaded(self.models[language])
-        nlp_engine = SpacyNlpEngine(models=[{"lang_code": language, "model_name": self.models[language]}])
-        analyzer = AnalyzerEngine(nlp_engine=nlp_engine, supported_languages=[language])
-        analyzer.registry.add_recognizer(SpacyRecognizer(supported_language=language, supported_entities=entities))
-        return analyzer
-
-    def _ensure_model_loaded(self,model_name):
-        if model_name in self.models_loaded:
+    def _get_analyzer(self,language):
+        model_name = self.models[language]
+        if model_name in self.models_loaded.keys():
             print(f"Model '{model_name}' already loaded.")
-            return
+            return self.models_loaded[model_name]
         print(f"Loading model '{model_name}'.")
         try:
             # Try to load the model
-            return spacy.load(model_name)
+            self.models_loaded[model_name] = spacy.load(model_name)
+            print(f"Model '{model_name}' loaded.")
+            return self.models_loaded[model_name]
         except OSError:
             # If the model is not found, download it
             print(f"Model '{model_name}' not found. Downloading...")
             spacy.cli.download(model_name)
             print(f"Model '{model_name}' downloaded successfully.")
-            return spacy.load(model_name)
-        finally:
-            self.models_loaded.append(model_name)
-            print(f"Model '{model_name}' loaded.")
+            return self._get_analyzer(model_name)
 
 if __name__ == "__main__":
     if "loadModels" in sys.argv:
         anon = Anon_Spacy()
         for model in anon.models.values():
-            anon._ensure_model_loaded(model)
+            anon._get_analyzer(model)
         print("All models loaded.")
--- tests/utils/test_backends.py
+++ tests/utils/test_backends.py
@@ -3,25 +3,22 @@ from src.utils.ano_spacy import Anon_Spacy
 from src.utils.ano_regex import find_entities as reg_find_entities
 from src.utils.ano_llm import find_entities as llm_find_entities
 
-testees = [
-            reg_find_entities, 
-            llm_find_entities,
-        #    Anon_Spacy().find_entities
-           ]
-
-
 if __name__ == "__main__":
-    print(repr(llm_find_entities))
-    print(llm_find_entities("Hello there"))
+    print(Anon_Spacy().find_entities("Hello there"))
 
 
 @pytest.mark.parametrize("input,expected", [
     ("Hello there", {'text': 'Hello there','replace_dict': {}}),
+    #("Hallo Roberto, how are you?", {'text': 'Hallo #PERSON_1#, how are you?','replace_dict': {'#PERSON_1#': {'matches': {'Roberto'}, 'replacement': 'Roberto'}}}),
 ])
-def test_backend( input: str, expected: dict):
-    for index, testee in enumerate(testees):
-        print(f"Iteration {index}")
-        print(f"Testing {testee.__name__} with input '{input}'")
-        result = testee(input)
-        print(f"Result: {result}")
-        assert result == expected
+@pytest.mark.parametrize("testee", [
+    (reg_find_entities),
+    (llm_find_entities),
+    (Anon_Spacy().find_entities),
+])
+@pytest.mark.filterwarnings("ignore:PluggyTeardownRaisedWarning")
+def test_backend(testee, input: str, expected: dict):
+    print(f"Testing {testee.__name__} with input '{input}'")
+    result = testee(input)
+    print(f"Result: {result}")
+    assert result == expected
