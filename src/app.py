fbacee7515e0f11249acfc763fdf66427c604897
robbizbal <roberto.galvagno@baloise.com>
2024-10-31 13:34:47 +0100

    overhaul gui

--- src/app.py
+++ src/app.py
@@ -1,9 +1,10 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 
-from src.gui import landing 
+from src.gui import gui_landing, gui_mask, gui_demaks
 from src.api import health
 from src.api import mask
+from src.api import demask
 
 app = FastAPI()
 
@@ -11,9 +12,16 @@ app = FastAPI()
 app.mount("/static", StaticFiles(directory="src/static"), name="static")
 
 # include routers
-app.include_router(landing.router)
+## GUI
+app.include_router(gui_landing.router)
+app.include_router(gui_mask.router)
+app.include_router(gui_demaks.router)
+
+## API
 app.include_router(health.router, prefix="/api")
 app.include_router(mask.router, prefix="/api")
+app.include_router(demask.router, prefix="/api")
+
 
 # run the app if this file is executed
 if __name__ == "__main__":
---
+++ src/gui/gui_mask.py
@@ -0,0 +1,14 @@
+import fastapi
+from fastapi.responses import HTMLResponse
+from fastapi import Request, routing
+from fastapi.templating import Jinja2Templates
+
+templates = Jinja2Templates(directory="src/templates/html")
+
+router = fastapi.APIRouter()
+
+@router.get("/mask", response_class=HTMLResponse, include_in_schema=False)
+def landing(request: Request): 
+    return templates.TemplateResponse(
+        request=request, name="mask.html", context={"id": id}
+    )
