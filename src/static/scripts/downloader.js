38b6e866a53f1e1b9e7380e9de210c627cf95f4f
robbizbal <roberto.galvagno@baloise.com>
2024-10-31 09:18:54 +0100

    Minor UI changes

--- src/static/scripts/downloader.js
+++ src/static/scripts/downloader.js
@@ -1,18 +1,26 @@
 document.getElementById('downloadBtn').addEventListener('click', function() {
     // Get the content of the textarea
     const foundEntities = document.getElementById('responseFieldEntities').value;
+    const newText = document.getElementById('responseFieldAnonText').value;
 
     // Create a Blob from the textarea content
-    const blob = new Blob([foundEntities], { type: 'application/json' });
+    const blobEntities = new Blob([foundEntities], { type: 'application/json' });
+    const blobNewText = new Blob([newText], { type: 'text/plain' });
 
     // Create a link element
-    const link = document.createElement('a');
-    link.href = URL.createObjectURL(blob);
-    link.download = 'yoyo-entities.json'; // Specify the file name
+    const linkE = document.createElement('a');
+    linkE.href = URL.createObjectURL(blobEntities);
+    linkE.download = 'yoyo-entities.json'; // Specify the file name
+
+    const linkNT = document.createElement('a');
+    linkNT.href = URL.createObjectURL(blobNewText);
+    linkNT.download = 'yoyo-anonymizedText.txt'; // Specify the file name
 
     // Programmatically click the link to trigger the download
-    link.click();
+    linkE.click();
+    linkNT.click();
 
     // Clean up and revoke the object URL
-    URL.revokeObjectURL(link.href);
+    URL.revokeObjectURL(linkE.href);
+    URL.revokeObjectURL(linkNT.href)
 });
