afdf327f817581ceff78ac36ed4df2b254d9606d
robbizbal <roberto.galvagno@baloise.com>
2024-11-01 08:53:19 +0100

    Fix demasker
    Fixed for new output/result format

--- src/utils/replacer.py
+++ src/utils/replacer.py
@@ -1,16 +1,4 @@
-import json
-
-def revert_replacements(text, replacements) -> str:
-    reversed_replacements = {}
-    
-    for key, values in replacements.items():
-        for value in values:
-            if value not in reversed_replacements:
-                reversed_replacements[value] = []
-            reversed_replacements[value].append(key)
-    
-    for value, keys in reversed_replacements.items():
-        for key in keys:
-            text = text.replace(key, value['matches'])
-    
-    return text
+def revert_replacements(masked_text, entity_dict):
+    for key, value in entity_dict.items():
+        masked_text = masked_text.replace(key, value['replacement'])
+    return masked_text
